e75fa4e1b9b925600db2fa8bf3a22b23476479c9
Surbhi Mittal <65413535+surbhi-mittal22@users.noreply.github.com>
2020-06-17 18:17:06 +0530

    Add files via upload

---
+++ dataStructure/binarySearchTree.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  	
+  	void preOrder(Node *root) {
+		
+      	if( root == NULL )
+          	return;
+      
+      	std::cout << root->data << " ";
+      	
+      	preOrder(root->left);
+      	preOrder(root->right);
+    }
+
+/*
+Node is defined as 
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+            Node * insert(Node * root, int data) {
+        if (!root)  return new Node(data);
+        Node* base=root, *prev;
+        while(base)
+        {
+            prev=base;
+            if (data<base->data)
+                base=base->left;
+            else base=base->right;
+        }
+        if (prev->data<data)    prev->right = new Node(data);
+        else prev->left = new Node(data);
+        return root;
+    }
+
+
+        
+
+};
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  	
+    myTree.preOrder(root);
+  
+    return 0;
+}
